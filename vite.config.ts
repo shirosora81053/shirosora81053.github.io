28ac2502d0f16e6abdf4b6a800576eafbdc88b44
shirosora81053 <shiroganesora@outlook.jp>
2026-03-25 12:48:49 +0900

    PrimeVueのインストール

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 import Components from 'unplugin-vue-components/vite'
+
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next/resolvers'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     Components({
-      resolvers: [BootstrapVueNextResolver()],
+      resolvers: [BootstrapVueNextResolver(), PrimeVueResolver()],
     }),
   ],
   resolve: {
