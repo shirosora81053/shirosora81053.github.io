60fd298399e1f3522529000485412c7095b151c7
shirosora81053 <shiroganesora@outlook.jp>
2026-03-25 11:35:22 +0900

    BootstrapVueNextのインストール

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
