9117128181f9ee072c60d8d3e8ffb9431cb47fce
shirosora81053 <shiroganesora@outlook.jp>
2026-03-25 11:41:41 +0900

    BootstrapIconsのインストール

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
